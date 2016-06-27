186fbbe0d1d5a04c0f00b5fea8fb0dfb29b64034
dht <dhtrovik@gmail.com>
2016-06-27 16:11:17 -0600

    Finished the implementation of GPS-Performance in the Performance (copy).sh file

--- Documents/Test12h/ShScripts/Performance (copy).sh
+++ Documents/Test12h/ShScripts/Performance (copy).sh
@@ -31,32 +31,70 @@ echo " " >> Report.txt
 echo "- MAX2769 Sampfreq:6864e6 26min x86 StaticSim ----------------------------------" >> Report.txt
 echo " " >> Report.txt
 
-max3dval=`awk 'NR == 1 {print $5  meters		 $9  meters		$11 meters}' /home/dlc257/Documents/resultOver_0_0.txt | sed 's/.*|//'`
-echo "Max 3D Error:	$max3dval" >> Report.txt
-mean3dval=`awk 'NR == 2 {print $4 " meters		" $6 " meters		" $8 " meters"}' /home/dlc257/Documents/resultOver_0_0.txt | sed 's/.*|//'`
-echo "Mean 3D Error:	$mean3dval" >> Report.txt
+sed -i 's/|//g' StaticSim/Plots/resultOver_0_0.txt
+sed -i 's/"//g' StaticSim/Plots/resultOver_0_0.txt 
+max3dval=`awk '/Max 3D Error/ {print $4, " meters	", $8, " meters	", $10, " meters"}' ./StaticSim/Plots/resultOver_0_0.txt`
+echo "Max 3D Error:		$max3dval" >> Report.txt
+mean3dval=`awk '/Mean 3D/ {print $3, " meters	", $5, " meters	", $7, " meters"}' ./StaticSim/Plots/resultOver_0_0.txt`
+echo "Mean 3D Error:		$mean3dval" >> Report.txt
+availabilityval=`awk '/Availability/ {print $2, " %			", $4, " %			", $6, " %"}' /StaticSim/Plots/resultOver_0_0.txt`
+echo "Availability:       	$availabilityval" >> Report.txt
 
 echo " " >> Report.txt
 echo "- MAX2769 Sampfreq:6864e6 52min x86 Static -------------------------------------" >> Report.txt
 echo " " >> Report.txt
 
+sed -i 's/|//g' Static/Plots/resultOver_0_0.txt
+sed -i 's/"//g' Static/Plots/resultOver_0_0.txt 
+max3dval=`awk '/Max 3D Error/ {print $4, " meters	", $8, " meters	", $10, " meters"}' ./Static/Plots/resultOver_0_0.txt`
+echo "Max 3D Error:		$max3dval" >> Report.txt
+mean3dval=`awk '/Mean 3D/ {print $3, " meters	", $5, " meters	", $7, " meters"}' ./Static/Plots/resultOver_0_0.txt`
+echo "Mean 3D Error:		$mean3dval" >> Report.txt
+availabilityval=`awk '/Availability/ {print $2, " %			", $4, " %			", $6, " %"}' ./Static/Plots/resultOver_0_0.txt`
+echo "Availability:       	$availabilityval" >> Report.txt
+
 echo " " >> Report.txt
 echo "- MAX2769 Sampfreq:6864e6 52min ARM Static -------------------------------------" >> Report.txt
 echo " " >> Report.txt
+
+sed -i 's/|//g' /6TB/nfsshare/nightly-results/resultOver_0_0.txt
+sed -i 's/"//g' /6TB/nfsshare/nightly-results/resultOver_0_0.txt 
+max3dval=`awk '/Max 3D Error/ {print $4, " meters	", $8, " meters	", $10, " meters"}' /6TB/nfsshare/nightly-results/resultOver_0_0.txt`
+echo "Max 3D Error:		$max3dval" >> Report.txt
+mean3dval=`awk '/Mean 3D/ {print $3, " meters	", $5, " meters	", $7, " meters"}' /6TB/nfsshare/nightly-results/resultOver_0_0.txt`
+echo "Mean 3D Error:		$mean3dval" >> Report.txt
+availabilityval=`awk '/Availability/ {print $2, " %			", $4, " %			", $6, " %"}' /6TB/nfsshare/nightly-results/resultOver_0_0.txt`
+echo "Availability:       	$availabilityval" >> Report.txt
 
 echo " " >> Report.txt
 echo "- MAX2769 Sampfreq:6864e6 34hours x86 StaticLong -------------------------------" >> Report.txt
 echo " " >> Report.txt
 
+sed -i 's/|//g' StaticLong/Plots/resultOver_0_0.txt
+sed -i 's/"//g' StaticLong/Plots/resultOver_0_0.txt 
+max3dval=`awk '/Max 3D Error/ {print $4, " meters	", $8, " meters	", $10, " meters"}' ./StaticLong/Plots/resultOver_0_0.txt`
+echo "Max 3D Error:		$max3dval" >> Report.txt
+mean3dval=`awk '/Mean 3D/ {print $3, " meters	", $5, " meters	", $7, " meters"}' ./StaticLong/Plots/resultOver_0_0.txt`
+echo "Mean 3D Error:		$mean3dval" >> Report.txt
+availabilityval=`awk '/Availability/ {print $2, " %			", $4, " %			", $6, " %"}' ./StaticLong/Plots/resultOver_0_0.txt`
+echo "Availability:       	$availabilityval" >> Report.txt
+
 echo " " >> Report.txt
 echo "- URSP-N210 Sampfreq:4e6 54min x86 Dynamic -------------------------------------" >> Report.txt
 echo " " >> Report.txt
 
-
+sed -i 's/|//g' Dynamic/Plots/resultOver_0_0.txt
+sed -i 's/"//g' Dynamic/Plots/resultOver_0_0.txt 
+max3dval=`awk '/Max 3D Error/ {print $4, " meters	", $8, " meters	", $10, " meters"}' ./Dynamic/Plots/resultOver_0_0.txt`
+echo "Max 3D Error:		$max3dval" >> Report.txt
+mean3dval=`awk '/Mean 3D/ {print $3, " meters	", $5, " meters	", $7, " meters"}' ./Dynamic/Plots/resultOver_0_0.txt`
+echo "Mean 3D Error:		$mean3dval" >> Report.txt
+availabilityval=`awk '/Availability/ {print $2, " %			", $4, " %			", $6, " %"}' ./Dynamic/Plots/resultOver_0_0.txt`
+echo "Availability:       	$availabilityval" >> Report.txt
 
 ## Prints Completion Message
 echo "#########################################################################" >> Report.txt
 echo "###########   Performance Characteristics Summary Complete   ############" >> Report.txt
 echo "#########################################################################" >> Report.txt
 
-sleep 5m
+sleep 5s
