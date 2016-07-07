45818d1ff16099b867a933972d8796f23359cbda
dhtorvik <dhtorvik@gmail.com>
2016-07-07 13:59:31 -0600

    Commented timeFormat and PerfCmp and changed dynamic swc file to include L2C

--- Documents/Test12h/ShScripts/PerfCmp.sh
+++ Documents/Test12h/ShScripts/PerfCmp.sh
@@ -1,6 +1,20 @@
+#!/bin/bash
+#################################################
+#      		   PerfCmp			#
+# Checks if the performance is better, normal, 	#
+# worse, or better and worse (warning).		#
+#						#
+# Input: the difference in mean3dval and the 	#
+# 	 the difference in availability.	#
+#						#
+# Output:  Performance message			#
+#################################################
+
+# Takes in arguments
 mean3dval=$1
 availability=$2
 
+# If either of them do not exist, return Test Not Run
 if [ -v $mean3dval ] || [ -v $availability ]
 	then
 	echo "Test Not Run"
@@ -11,11 +25,14 @@ fi
 mean3dval=`echo ${mean3dval} | sed -e 's/[eE]+*/\\*10\\^/'`
 availability=`echo ${availability} | sed -e 's/[eE]+*/\\*10\\^/'`
 
+# If mean3dval is less than 0.2 but positive then it is set to zero since it is not that bad
 if [ $(echo "$mean3dval < 0.2" | bc -l) -eq 1 ] && [ $(echo "$mean3dval > 0" | bc -l) -eq 1 ]
 	then
 	mean3dval=0
 fi
 
+# Checks whether mean3dval is better, same, or worse and then checks whether availability is
+# better, same, or worse. Depending on situation, it will print out message.
 if [ $(echo "$mean3dval < 0" | bc -l) -eq 1 ]
 	then
 	if [ $(echo "$availability <= 0" | bc -l) -eq 1 ]
--- Documents/Test12h/ShScripts/timeFormat.sh
+++ Documents/Test12h/ShScripts/timeFormat.sh
@@ -1,20 +1,37 @@
+#!/bin/bash
+#################################################
+#      		   timeFormat			#
+# Takes in time as seconds and outputs it into	#
+# hh:mm:ss format. Also has capability for +/-	#
+# being placed at the beginning of the time.	#
+#						#
+# Input: time in seconds			#
+#						#
+# Output:  time in hh:mm:ss with +/- possible	#
+#################################################
+
+# Takes in arguments
 diff=$1
 plmi=$2
 
+# If there is nothing inputted then it returns dashed line
 if [ -v $diff ] || [ $diff == "-" ]
 	then
 	echo "--------"
 	exit
 fi
 
+# If it is less than zero, then it adds a minus sign to the beginning
 if [ $diff -lt 0 ]
 	then
 	diff=`expr -1 \* $diff`
 	plmi=-
 fi
+# Finds exactly how many hours, minutes, and seconds there are
 hours=$(($diff / 3600))
 min=$(($(($diff / 60)) % 60))
 sec=$(($diff % 60))
+# If either hours, minutes, or seconds are less than ten then it adds a zero for formatting
 if [ $hours -lt 10 ]
 	then
 	hours=0$hours
@@ -27,5 +44,5 @@ if [ $sec -lt 10 ]
 	then
 	sec=0$sec
 fi
-
+# Returns it in the correct format
 echo "$plmi$hours:$min:$sec"
